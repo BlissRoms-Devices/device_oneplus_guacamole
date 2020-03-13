c61144f7f724b93c55ce6126ef4124d6906d1098
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2020-03-13 18:24:36 -0400

    Let's give these a try

--- bliss_guacamole.mk
+++ bliss_guacamole.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/oneplus/guacamole/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
+# Inherit PixelGApps
+$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := bliss_guacamole
 PRODUCT_DEVICE := guacamole
