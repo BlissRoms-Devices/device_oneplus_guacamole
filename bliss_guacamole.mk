4d5da08f4ab649acaecff5fe5df91c611a5a6e21
srgrusso <srgrusso240@gmail.com>
2022-06-18 10:47:13 -0500

    guacamole: Add QUICK_TAP flag

--- bliss_guacamole.mk
+++ bliss_guacamole.mk
@@ -17,6 +17,7 @@ $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := bliss_guacamole
